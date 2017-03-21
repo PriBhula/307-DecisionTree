5da1331d9013cb48889ae308bf9e9e635f061a45
Pri Bhula <pridotbhula@gmail.com>
2017-03-21 14:32:13 +1300

    Initial commit

---
+++ src/BuildTree.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Pri on 21/03/2017.
+ */
+
+import java.util.List;
+import java.util.Set;
+
+public class BuildTree{
+
+    public void decTreeALg (Set instances, List attributes){
+
+        if (instances.isEmpty()){
+            //return leaf node with name and prob of the overall most probabl class
+        }
+
+        //if instances are pure
+
+        if (attributes.isEmpty()){
+            //return leaf node with name and prob of major. class of instance
+        }
+
+        //else find find best attribute
+
+    }
+}
